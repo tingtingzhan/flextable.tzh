6fb8c19ce0f57a460111c9c8a1e786608d2eed58
Tingting Zhan <tingtingzhan@gmail.com>
2025-02-26 20:34:18 -0500

    test

--- R/pairwise.htest.R
+++ R/pairwise.htest.R
@@ -1,11 +1,17 @@
 
-#' @title as_flextable.pairwise.htest
+#' @title Convert \eqn{p}-values of `pairwise.htest` Object into \link[flextable]{flextable}
 #' 
-#' @param x a `'pairwise.htest'` object, as returned from \link[stats]{pairwise.t.test},
+#' @description
+#' Convert \eqn{p}-values of a `pairwise.htest` object into a \link[flextable]{flextable}.
+#' 
+#' @param x a `'pairwise.htest'` object, as returned from functions \link[stats]{pairwise.t.test},
 #' \link[stats]{pairwise.wilcox.test} or
 #' \link[stats]{pairwise.prop.test}.
 #' 
-#' @param row.title,... additional parameters of [as_flextable.array]
+#' @param row.title,... additional parameters of function [as_flextable.array]
+#' 
+#' @returns
+#' Function [as_flextable.pairwise.htest] returns a \link[flextable]{flextable}.
 #' 
 #' @examples
 #' aq = within(airquality, expr = {
