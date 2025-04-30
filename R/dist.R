fb0363b19220e2ca044bea0227ef9c9cab88d531
Tingting Zhan <tingtingzhan@gmail.com>
2025-04-29 23:01:40 -0400

    tzh

--- R/dist.R
+++ R/dist.R
@@ -16,15 +16,16 @@
 #' @export as_flextable.dist
 #' @export
 as_flextable.dist <- function(x, ...) {
-  # do not overwrite ?stats:::format.dist
   # inspired by ?stats:::print.dist
-  Labels <- attr(x, which = 'Labels', exact = TRUE)
-  Size <- attr(x, which = 'Size', exact = TRUE)
-  if (!length(Labels)) Labels <- sprintf(fmt = '[%d]', seq_len(Size))
+  Labels <- x |> attr(which = 'Labels', exact = TRUE)
+  Size <- x |> attr(which = 'Size', exact = TRUE)
+  if (!length(Labels)) 
+    Labels <- Size |> seq_len() |> sprintf(fmt = '[%d]')
   .dim <- c(Size, Size)
   ret <- array('', dim = .dim, dimnames = list(Labels, Labels))
   lo <- .row(.dim) - .col(.dim) # ?base::lower.tri
   ret[lo > 0L] <- format(x, ...) # ?stats:::format.dist
-  as_flextable.matrix(ret[-1L, -Size, drop = FALSE], ...)
+  ret[-1L, -Size, drop = FALSE] |>
+    as_flextable.matrix(...)
 }
 
